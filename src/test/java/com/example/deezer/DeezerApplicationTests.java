bf60f0de151cc54c75cf72ac26096445206a8fd1
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-02-11 17:57:24 +0500

    Initial commit

---
+++ src/test/java/com/example/deezer/DeezerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deezer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeezerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
