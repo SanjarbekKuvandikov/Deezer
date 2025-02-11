8b555084b20a2aaab89a7ab220a9112963560d33
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-02-12 00:22:40 +0500

    data base added

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,19 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	//json
+	implementation ("com.google.code.gson:gson:2.8.9")
+	//
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	//base
+	implementation("org.postgresql:postgresql:42.7.3")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.2")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc:3.3.2")
+
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
