9d8afacef7978eeb656ae7985d2ac8b1c1c9cc1b
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-02-12 16:14:15 +0500

    SongController class added

--- src/main/java/com/example/deezer/controller/SongController.java
+++ src/main/java/com/example/deezer/controller/SongController.java
@@ -3,6 +3,8 @@ package com.example.deezer.controller;
 import com.example.deezer.entity.Song;
 import com.example.deezer.repository.SongsRepository;
 import com.example.deezer.service.DeezerService;
+import com.example.deezer.service.SongService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,10 +14,12 @@ import java.util.List;
 public class SongController {
     private final SongsRepository songsRepository;
     private final DeezerService deezerService;
+    private final SongService songService;
 
-    public SongController(SongsRepository songsRepository, DeezerService deezerService) {
+    public SongController(SongsRepository songsRepository, DeezerService deezerService, SongService songService) {
         this.songsRepository = songsRepository;
         this.deezerService = deezerService;
+        this.songService = songService;
     }
 
     //find song and save
@@ -25,18 +29,23 @@ public class SongController {
         return songsRepository.save(song);
     }
 
-    @PutMapping("/{id}")
-    public Song updateURL(@PathVariable Long id, @RequestParam String url){
-        return songsRepository.findById(id).stream().map( song -> {
-            song.setUrl(url);
-            return songsRepository.save(song);
-        }).findAny().orElse(null);
-    }
-
     //Get all songs
     @GetMapping
     public List<Song> getAll(){
         return songsRepository.findAll();
+    }
+
+    //put url
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateURL(@PathVariable Long id, @RequestParam String url){
+        songService.updateSongUrl(id,url);
+        return ResponseEntity.ok("Song`s url updated successfully");
+    }
+
+    //getting no url
+    @GetMapping("/no-url")
+    public List<Song> getSongsWithoutUrl(){
+     return songsRepository.findByUrlIsNullOrUrl("");
     }
 
 }
