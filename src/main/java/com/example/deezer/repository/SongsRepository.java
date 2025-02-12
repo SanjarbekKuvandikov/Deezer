650d9c63f6a4d266ebb04b9df56b7473aeddd80c
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-02-12 16:15:15 +0500

    SongsRepository updated

--- src/main/java/com/example/deezer/repository/SongsRepository.java
+++ src/main/java/com/example/deezer/repository/SongsRepository.java
@@ -3,6 +3,8 @@ package com.example.deezer.repository;
 import com.example.deezer.entity.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SongsRepository  extends JpaRepository<Song,Long> {
+import java.util.List;
 
+public interface SongsRepository  extends JpaRepository<Song,Long> {
+    List<Song> findByUrlIsNullOrUrl(String url);
 }
