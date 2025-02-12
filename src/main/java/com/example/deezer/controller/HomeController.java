d8037f5ec88d9b09f87db9efaac9c7bb900dfb47
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-02-12 16:13:54 +0500

    HomeController class added

---
+++ src/main/java/com/example/deezer/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.deezer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/songsList")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping("allList")
+    public String all() {
+        return "all";
+    }
+
+    @GetMapping("no_url")
+    public String noUrl() {
+        return "no_url";
+    }
+
+}
