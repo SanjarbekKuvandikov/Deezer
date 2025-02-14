3862c04e9487cbcd5b3d6f59f8d5d485a9c21a78
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-02-14 21:33:23 +0500

    Artist and Song data classes created to save info that came by deezer api

---
+++ src/main/java/com/example/deezer/dto/ArtistData.java
@@ -0,0 +1,10 @@
+package com.example.deezer.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ArtistData {
+    private String name;
+}
---
+++ src/main/java/com/example/deezer/dto/SongData.java
@@ -0,0 +1,12 @@
+package com.example.deezer.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class SongData {
+    private String title;
+    private ArtistData artist;
+
+}
